d7cf206c45d613d7f42bf9e151a9eddf68b4614b
Angel <angelmejiaprz@gmail.com>
2025-02-13 19:53:03 -0600

    crud de usuarios creado listo para trabajar

---
+++ users/src/test/java/com/declarationsystem/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.declarationsystem.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
